a406001094fc3c7eed7a0b88c9a30ce04b17e840
DragonMess <kmylop@hotmail.com>
2020-09-01 17:38:49 +0000

    user_spec test added

---
+++ spec/user_spec.rb
@@ -0,0 +1,11 @@
+RSpec.describe User, type: :model do
+
+  describe 'Validations' do
+    # validation examples here
+  end
+
+  describe '.authenticate_with_credentials' do
+    # examples for this class method here
+  end
+
+end
